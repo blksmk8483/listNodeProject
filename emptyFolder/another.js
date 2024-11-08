80a0cbd03be62e2643001a890f451d39ac0acb49
Brandon Knight <knightbrandon1984@gmail.com>
2024-11-08 12:42:06 -0600

    fixed the bug with folders switching order

---
+++ emptyFolder/another.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const util = require("util");
+
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+  for (let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
